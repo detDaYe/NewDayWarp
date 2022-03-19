71a4779a1501e7837c2854e262cf2ceb5eaab344
Romeo RKPK <mindaye8504@gmail.com>
2022-03-19 21:59:15 +0900

    RegionLeftListener created

---
+++ src/main/java/org/lumenk/newdaywarp/listeners/RegionLeftListener.java
@@ -0,0 +1,13 @@
+package org.lumenk.newdaywarp.listeners;
+
+import net.raidstone.wgevents.events.RegionLeftEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class RegionLeftListener implements Listener {
+
+    @EventHandler
+    public void onRegionLeft(RegionLeftEvent event){
+
+    }
+}
