1e3bddafeb07d4c3ef8332a50c360f8a1f5bcdb7
Romeo RKPK <mindaye8504@gmail.com>
2022-03-19 21:58:14 +0900

    enter event added

---
+++ src/main/java/org/lumenk/newdaywarp/listeners/RegionEnterListener.java
@@ -0,0 +1,13 @@
+package org.lumenk.newdaywarp.listeners;
+
+import net.raidstone.wgevents.events.RegionEnteredEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class RegionEnterListener implements Listener {
+
+    @EventHandler
+    public void onRegionEnter(RegionEnteredEvent event){
+
+    }
+}
