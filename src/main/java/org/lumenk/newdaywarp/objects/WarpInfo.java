ece18c9d48e8f7e28149efdb8daf4203056217ff
Romeo RKPK <mindaye8504@gmail.com>
2022-03-19 22:09:03 +0900

    work in progress

--- src/main/java/org/lumenk/newdaywarp/objects/WarpInfo.java
+++ src/main/java/org/lumenk/newdaywarp/objects/WarpInfo.java
@@ -1,8 +1,14 @@
 package org.lumenk.newdaywarp.objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class WarpInfo {
 
     @NotNull
