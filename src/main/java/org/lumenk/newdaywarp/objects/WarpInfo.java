a9a8348190548f87c59d91faa2457ad71a84f594
Romeo RKPK <mindaye8504@gmail.com>
2022-03-19 22:08:13 +0900

    work in progress

---
+++ src/main/java/org/lumenk/newdaywarp/objects/WarpInfo.java
@@ -0,0 +1,14 @@
+package org.lumenk.newdaywarp.objects;
+
+import org.bukkit.Location;
+import org.jetbrains.annotations.NotNull;
+
+public class WarpInfo {
+
+    @NotNull
+    private String departureRegionName;
+
+    @NotNull
+    private Location arrivalLocation;
+
+}
